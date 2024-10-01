2df68ccda0e804eb4c6a3e5add1ce4a386817bd0
JuanG1986 <126287790+JuanG1986@users.noreply.github.com>
2024-10-01 17:42:46 +0200

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -56,9 +56,9 @@ global.img16 = 'https://qu.ax/FQZm.jpg' //+18
 global.img17 = 'https://qu.ax/OsWR.jpg'
 global.img18 = 'https://qu.ax/GGmA.jpg'
 
-global.logogit = 'https://qu.ax/RzKF.jpg'
+global.logogit = 'https://qu.ax/Wwog.jpg'
 
-global.gataVidMenu = ['https://qu.ax/AAvg.mp4', 'https://qu.ax/SeFS.mp4', 'https://qu.ax/NqZN.mp4'].getRandom()
+global.gataVidMenu = ['https://qu.ax/gdBw.mp4', 'https://qu.ax/SeFS.mp4', 'https://qu.ax/NqZN.mp4'].getRandom()
 global.gataMenu = [img, img2, img6, img7, img8, img9, img13, img14, img15, img17, img18].getRandom()
 global.gataImg = [imagen1, imagen2, imagen3, imagen4, imagen5, imagen6, imagen7, imagen8, imagen9, imagen10, imagen11, imagen12, imagen13].getRandom()
 
