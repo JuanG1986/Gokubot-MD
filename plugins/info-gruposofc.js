e272ed6be2e0c15196ebb6ec42863f2a13b29079
JuanG1986 <126287790+JuanG1986@users.noreply.github.com>
2024-10-02 08:45:32 +0200

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -3,44 +3,64 @@ let media = gataVidMenu
 let str = `*◜💙 GRUPOS OFICIALES 💙◞*
 
 🌍📱🎮🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕃𝕖𝕘𝕖𝕟𝕕𝕤 🐉 🎮📱🌍
+https://chat.whatsapp.com/L2vpoClRqYC6wsmGC0gFqS
 
 🌍🐉👨‍👩‍👧‍👧 𝔽𝕒𝕞𝕚𝕝𝕚𝕒 𝕊𝕒𝕚𝕪𝕒𝕟 👨‍👩‍👧‍👧🐉🌍
+https://chat.whatsapp.com/LmDvJZWd0eQCzhR8lwJ5B4
 
 🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝔽𝕠𝕣𝕥𝕟𝕚𝕥𝕖 🐉
+https://chat.whatsapp.com/KJcE87Yb8ZP7WDiww8UDkY
 
 🌍🐉🐲 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 ℝ𝕠𝕝 🐲🐉🌍
+https://chat.whatsapp.com/JXVf0iaQhuvE7uQCE8QSym
 
 🌍🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝔸𝔽 🐉🌍
+https://chat.whatsapp.com/EJFl28xgwLeEhIQxriWiuZ
 
 🌍🐉🦹‍♂️ 𝕍𝕚𝕝𝕝𝕒𝕟𝕠𝕤 𝕕𝕖 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 🐉🦹‍♂️🌍
+https://chat.whatsapp.com/LCF5v7bb9dN8lGOrY9OlFR
 
 🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕏𝕖𝕟𝕠𝕧𝕖𝕣𝕤𝕖 🐉
+https://chat.whatsapp.com/Hq0VCaXqGaU40P89HmPuni
 
 🌍🐉📹 𝕍𝕚𝕕𝕖𝕠𝕤 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 📹🐉🌍
+https://chat.whatsapp.com/EaTmnGSAX3pH06Dl7MRzmV
 
 🌍🐲🐉𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕊𝕥𝕚𝕔𝕜𝕖𝕣𝕤🐉🐲🌍
+https://chat.whatsapp.com/K1FSWdgwuzTL1z08NVYJNw
 
 🌍🐉🎨 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕕𝕚𝕓𝕦𝕛𝕠𝕤 🎨🐉🌍
+https://chat.whatsapp.com/LddDN9xDXcg15IuOirylqZ
 
 🌍🐉 ℙ𝕠𝕨𝕖𝕣 𝕆𝕗 𝔾𝕠𝕕𝕤 🐉🌍
+https://chat.whatsapp.com/ELy0mvOw5JyLnHmwFY7j1K
 
 🌍📊🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 𝕖𝕟𝕔𝕦𝕖𝕤𝕥𝕒𝕤 🐉📊🌍
+https://chat.whatsapp.com/GX6QQjlGEcYAGz823ZUpfI
 
 🌍🎮🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝕓𝕒𝕝𝕝 𝕜𝕒𝕜𝕒𝕣𝕠𝕥 🐉🎮🌍
+https://chat.whatsapp.com/JbIgLHQuvue7hItxhng9nN
 
 🌍🎮🐉 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 ℤ 𝔻𝕠𝕜𝕜𝕒𝕟 𝕓𝕒𝕥𝕥𝕝𝕖 🐉🎮🌍
+https://chat.whatsapp.com/GEj9B5TlTvoIC6wBLJ71UU
 
 🐲🅳𝗥𝗔𝗚𝗢𝗡 🅱𝗔𝗟𝗟𝗦 🆉🐲🔥 🅵𝗔𝗡𝗦 🎥
+https://chat.whatsapp.com/KljiSB549Xc7HXXIzVPFat
 
 🌍🐉🐲 𝔽𝕒𝕟𝕤 𝔻𝕖 𝔻𝕣𝕒𝕘𝕠𝕟 𝔹𝕒𝕝𝕝 ℤ 𝕂𝕒𝕚 🐲🐉🌍
+https://chat.whatsapp.com/BhXwjZVP0Ae9VU4pHqXATS
 
 🐉🎮𝔻ℝ𝔸𝔾𝕆ℕ 𝔹𝔸𝕃𝕃 𝔽𝕀𝔾𝕋ℍ𝔼ℝℤ🎮🐉
+https://chat.whatsapp.com/LUKhODoKfqrC0uVKqMdfu4
 
 🌍🐉🐲 Noticias dragón ball 🐲🐉🌍
+https://whatsapp.com/channel/0029VaSZsS14Y9lhQYCFvO0w
 
 🌍🐉🐲 Curiosidad dragón ball 🐲🐉🌍
+https://whatsapp.com/channel/0029VaQghZyAInPcWuTHOR3V
 
 🌍🐉🐲 Dragón ball juegos 🐲🐉🌍
+https://whatsapp.com/channel/0029VaQkf7T0rGiORXhKqR0R
 
 By Juan1986`
 await conn.sendButton(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎𝙄𝙎𝙏𝙀𝙉𝘾𝙄𝘼\n${asistencia}\n\n` + wm, media, [
@@ -54,7 +74,7 @@ await conn.sendButton(m.chat, str, `𝙂𝘼𝙏𝘼 𝘿𝙄𝙊𝙎 - 𝘼𝙎
 ['𝙑𝙤𝙡𝙫𝙚𝙧 𝙖𝙡 𝙈𝙚𝙣𝙪́ | 𝘽𝙖𝙘𝙠 𝙩𝙤 𝙈𝙚𝙣𝙪 ☘️', '/menu']
 ], m,)}*/
 
-handler.command = /^linkgc|grupos|gruposgatabot|gatabotgrupos|gruposdegatabot|groupofc|gruposgb|grupogb|groupgb$/i
+handler.command = /^linkgb|grupos|gruposgokubot|gruposwhatsApp$/i
 handler.exp = 33
 
 export default handler
