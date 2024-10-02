9bbbfeba1626487f2fe81a4bce842f0f130500a9
JuanG1986 <126287790+JuanG1986@users.noreply.github.com>
2024-10-02 12:11:16 +0200

    Update _content.js

--- plugins/_content.js
+++ plugins/_content.js
@@ -13,7 +13,7 @@ global.welgata = [tk, ig, yt2, yt2, ig, md, ig, yt, paypal, yt2, yt2, ig, fb, tk
 global.redesMenu = [canal1, canal2, canal3, canal4, soporteGB, grupo1, grupo2, grupo3, grupo4, grupo5, grupo6, md, ig, paypal, yt, asistencia, fb, tk].getRandom()
 global.accountsgb = [canal1, canal2, canal3, canal4, tk, ig, yt, paypal, fb, ths, md, asistencia].getRandom()
 
-global.canalIdGB = ["120363195436416919newsletter", "120363195436416919@newsletter", "120363195436416919@newsletter", "120363195436416919@newsletter"]
+global.canalIdGB = ["120363195436416919@newsletter", "120363195436416919@newsletter", "120363195436416919@newsletter", "120363195436416919@newsletter"]
 global.canalNombreGB = ["Dragón ball bots", "Dragón ball bots", "Dragón ball bots", "Dragón ball bots"]
 global.channelRD = await getRandomChannel()
 
